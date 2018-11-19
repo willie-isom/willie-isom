789f8945177e9df7e31e552d271434c24eb0fb39
willie-isom <45085048+willie-isom@users.noreply.github.com>
2018-11-19 16:09:00 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -9,63 +9,78 @@
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
-  $imageId = $json_obj->events[0]->message->id; //取得訊息編號
-  $url = 'https://api.line.me/v2/bot/message/'.$imageId.'/content';
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Authorization: Bearer BjX2HZt6/dkTkPYf5+qAEvjXy97/Udan1/bwJFMFv6Jqsgtxbm1HDFqvNekUrDiXxvI3+VyI2N4WOQ9/yjn5M8+fbnVHzs02lJVRp25yyjeG54SwQsWW+4M7Sivz8mYfh5gpP6myJgNJ7HsCGycDugdB04t89/1O/w1cDnyilFU='
-  ));
-
-  $json_content = curl_exec($ch);
-  curl_close($ch);
-  $imagefile = fopen($imageId.".jpeg", "w+") or die("Unable to open file!");
-  fwrite($imagefile, $json_content); 
-  fclose($imagefile); //將圖片存在server上
-			
-  $header[] = "Content-Type: application/json";
-  $post_data = array (
-	"requests" => array (
-	  array (
-		"image" => array (
-		  "source" => array (
-			"imageUri" => "http://139.59.123.8/class/willie-isom/".$imageId.".jpeg"
-		  )
-		),
-		"features" => array (
-		  array (
-			"type" => "TEXT_DETECTION",
-			"maxResults" => 1
-		  )
-		)
-	  )
-	)
-  );
-  $ch = curl_init('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCiyGiCfjzzPR1JS8PrAxcsQWHdbycVwmg');                                                                      
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));                                                                  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
-  $result = json_decode(curl_exec($ch));
-  $result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
-  $ans_txt = "這張發票沒用了，你又製造了一張垃圾";
-  foreach ($result_ary as $val) {
-	if($val == "JS-07510912"){
-	  $ans_txt = "恭喜您中獎啦，快分紅!!";
-	}
+  switch ($sender_txt) {
+    case "push":
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "text",
+						"text" => "Hello. This is push. You say ".$sender_txt
+					)
+				)
+			);
+      break;
+    case "location":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "location",
+						"title" => "my location",
+						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+						"latitude" => 35.65910807942215,
+						"longitude" => 139.70372892916203
+					)
+				)
+			);
+			break;
+		case "sticker":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "sticker",
+						"packageId" => "1",
+						"stickerId" => "1"
+					)
+				)
+			);
+			break;
+		case "button":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "template",
+						"altText" => "this is a buttons template",
+						"template" => array (
+							"type" => "buttons",
+							"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
+							"title" => "Menu",
+							"text" => "Please select",
+							"actions" => array (
+								array (
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=123"
+								),
+								array (
+									"type" => "postback",
+									"label" => "Add to cart",
+									"data" => "action=add&itemid=123"
+								)
+							)
+						)
+					)
+				)
+			);
+			break;
   }
-  $response = array (
-	"replyToken" => $sender_replyToken,
-	"messages" => array (
-	  array (
-		"type" => "text",
-		//"text" => $ans_txt
-		"text" => $result -> responses[0] -> fullTextAnnotation -> text
-	  )
-	)
-  );
-  
-  
+
   fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
   $header[] = "Authorization: Bearer BjX2HZt6/dkTkPYf5+qAEvjXy97/Udan1/bwJFMFv6Jqsgtxbm1HDFqvNekUrDiXxvI3+VyI2N4WOQ9/yjn5M8+fbnVHzs02lJVRp25yyjeG54SwQsWW+4M7Sivz8mYfh5gpP6myJgNJ7HsCGycDugdB04t89/1O/w1cDnyilFU=";
