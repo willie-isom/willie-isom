6d673c6f5e951656d13294a3113f179f34f82740
willie-isom <45085048+willie-isom@users.noreply.github.com>
2018-11-19 10:25:45 +0800

    Create receive.php

---
+++ receive.php
@@ -0,0 +1,7 @@
+<?php
+  $json_str = file_get_contents('php://input'); //接收 request的body
+  $json_obj = json_decode($json_str); //轉成json格式
+  
+  $myfile = fopen("Log.txt","w+") or die("Unable to open file!"); //設定一個Log.txt來印訊息
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str); //\xEF\xBB\xBF轉成utf8格式
+?>
