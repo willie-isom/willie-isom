9c36ef7a16698603d42072f078a3980e6ddbd385
willie-isom <45085048+willie-isom@users.noreply.github.com>
2018-11-22 17:11:50 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -9,79 +9,17 @@
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
-  switch ($sender_txt) {
-    case "push":
-			$response = array (
-				"replyToken" => $sender_replyToken,
-				"messages" => array (
-					array (
-						"type" => "text",
-						"text" => "Hello. This is push. You say ".$sender_txt
-					)
-				)
-			);
-      break;
-    case "location":
-			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
-			$response = array (
-				"replyToken" => $sender_replyToken,
-				"messages" => array (
-					array (
-						"type" => "location",
-						"title" => "my location",
-						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-						"latitude" => 35.65910807942215,
-						"longitude" => 139.70372892916203
-					)
-				)
-			);
-			break;
-		case "sticker":
-			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
-			$response = array (
-				"replyToken" => $sender_replyToken,
-				"messages" => array (
-					array (
-						"type" => "sticker",
-						"packageId" => "1",
-						"stickerId" => "1"
-					)
-				)
-			);
-			break;
-		case "button":
-			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
-			$response = array (
-				"replyToken" => $sender_replyToken,
-				"messages" => array (
-					array (
-						"type" => "template",
-						"altText" => "this is a buttons template",
-						"template" => array (
-							"type" => "buttons",
-							"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
-							"title" => "Menu",
-							"text" => "Please select",
-							"actions" => array (
-								array (
-									"type" => "postback",
-									"label" => "Buy",
-									"data" => "action=buy&itemid=123"
-								),
-								array (
-									"type" => "postback",
-									"label" => "Add to cart",
-									"data" => "action=add&itemid=123"
-								)
-							)
-						)
-					)
-				)
-			);
-			break;
-  }
-
-  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+  $response = array (
+    "replyToken" => $sender_replyToken,
+    "messages" => array (
+      array (
+        "type" => "text",
+        "text" => "Hello. You say". $sender_txt
+      )
+    )
+  );
+  
+ fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
   $header[] = "Authorization: Bearer BjX2HZt6/dkTkPYf5+qAEvjXy97/Udan1/bwJFMFv6Jqsgtxbm1HDFqvNekUrDiXxvI3+VyI2N4WOQ9/yjn5M8+fbnVHzs02lJVRp25yyjeG54SwQsWW+4M7Sivz8mYfh5gpP6myJgNJ7HsCGycDugdB04t89/1O/w1cDnyilFU=";
   $ch = curl_init("https://api.line.me/v2/bot/message/reply");
